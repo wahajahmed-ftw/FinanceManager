bdcac401d58a6d01dcf3407e3d78e5031db83e30
wahajahmed-ftw <wahajahmed55@live.com>
2025-03-11 23:21:26 +0500

    More Issues Resolved 3

--- app/dashboard/expenses/page.tsx
+++ app/dashboard/expenses/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 import { useState } from "react";
-import ExpenseForm from "./ExpenseForm/expenseForm";
+import ExpenseFormPopup from "./ExpenseForm/expenseForm";
 import ExpenseTable from "./ExpenseTable/expenseTable";
 
 export default function Expenses() {
   const [dirty,setDirty] = useState(false)
   return (
     <div>
-      <ExpenseForm setDirty={setDirty}/>
+      <ExpenseFormPopup setDirty={setDirty}/>
       <ExpenseTable dirty={dirty} setDirty={setDirty}/>
     </div>
   );
